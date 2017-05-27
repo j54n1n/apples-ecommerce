034c26e6803fb289897ce9b6442dc0551178dbb9
luca9294 <luca9294@hotmail.it>
2017-05-27 15:57:29 +0200

    InstallCert

--- certificate/InstallCert.java
+++ certificate/InstallCert.java
@@ -1,4 +1,4 @@
-// Downloaded 1/1/2011 from http://blogs.sun.com/andreas/resource/InstallCert.java
+package test7;
 
 /*
  * Copyright 2006 Sun Microsystems, Inc.  All Rights Reserved.
@@ -30,157 +30,172 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-
-
-import java.io.*;
-import java.net.URL;
-
-import java.security.*;
-import java.security.cert.*;
+/**
+ * Originally from:
+ * http://blogs.sun.com/andreas/resource/InstallCert.java
+ * Use:
+ * java InstallCert hostname
+ * Example:
+ *% java InstallCert ecc.fedora.redhat.com
+ */
 
 import javax.net.ssl.*;
+import java.io.*;
+import java.security.KeyStore;
+import java.security.MessageDigest;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+/**
+ * Class used to add the server's certificate to the KeyStore
+ * with your trusted certificates.
+ */
 public class InstallCert {
 
     public static void main(String[] args) throws Exception {
-	String host;
-	int port;
-	char[] passphrase;
-	    String a ="54.202.224.165:8443";
-	    String[] c = a.split(":");
-	    host = c[0];
-	    port = (c.length == 1) ? 443 : Integer.parseInt(c[1]);
+        String host;
+        int port;
+        char[] passphrase;
+        System.setProperty("javax.net.ssl.trustStore", "jssecacerts");
+        System.setProperty("javax.net.ssl.trustStorePassword", "Aa30011992");
+      
+            host = "https://54.202.224.165:8443/apples-ecommerce-ws/Category?wsdl";
+            passphrase = "Aa30011992".toCharArray();
+    
 
+        File file = new File("jssecacerts");
+        if (file.isFile() == false) {
+            char SEP = File.separatorChar;
+            File dir = new File(System.getProperty("java.home") + SEP
+                    + "lib" + SEP + "security");
+            file = new File(dir, "jssecacerts");
+            if (file.isFile() == false) {
+                file = new File(dir, "cacerts");
+            }
+        }
+        System.out.println("Loading KeyStore " + file.getAbsolutePath());
+        InputStream in = new FileInputStream(file);
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        System.out.print(KeyStore.getDefaultType().toString());
+        ks.load(in, "Aa30011992".toCharArray());
+        in.close();
 
+        SSLContext context = SSLContext.getInstance("TLS");
+        TrustManagerFactory tmf =
+                TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+        tmf.init(ks);
+        X509TrustManager defaultTrustManager = (X509TrustManager) tmf.getTrustManagers()[0];
+        SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
+        context.init(null, new TrustManager[]{tm}, null);
+        SSLSocketFactory factory = context.getSocketFactory();
 
-	File file = new File("jssecacerts");
-	if (file.isFile() == false) {
-	    char SEP = File.separatorChar;
-	    File dir = new File(System.getProperty("java.home") + SEP
-		    + "lib" + SEP + "security");
-	    file = new File(dir, "jssecacerts");
-	    if (file.isFile() == false) {
-		file = new File(dir, "cacerts");
-	    }
-	}
-	System.out.println("Loading KeyStore " + file + "...");
-	InputStream in = new FileInputStream(file);
-	KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-	ks.load(in, "Aa30011992".toCharArray());
-	in.close();
+        SSLSocket socket = (SSLSocket) factory.createSocket("54.202.224.165", 8443);
+        socket.setSoTimeout(10000);
+        try {
+            System.out.println("Starting SSL handshake...");
+            socket.startHandshake();
+            socket.close();
+            System.out.println();
+            System.out.println("No errors, certificate is already trusted");
+        } catch (SSLException e) {
+            System.out.println();
+            e.printStackTrace(System.out);
+        }
 
-	SSLContext context = SSLContext.getInstance("TLS");
-	TrustManagerFactory tmf =
-	    TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-	tmf.init(ks);
-	X509TrustManager defaultTrustManager = (X509TrustManager)tmf.getTrustManagers()[0];
-	SavingTrustManager tm = new SavingTrustManager(defaultTrustManager);
-	context.init(null, new TrustManager[] {tm}, null);
-	SSLSocketFactory factory = context.getSocketFactory();
+        X509Certificate[] chain = tm.chain;
+        if (chain == null) {
+            System.out.println("Could not obtain server certificate chain");
+            return;
+        }
 
-	System.out.println("Opening connection to " + host + ":" + port + "...");
-	SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
-	socket.setSoTimeout(10000);
-	try {
-	    System.out.println("Starting SSL handshake...");
-	    socket.startHandshake();
-	    socket.close();
-	    System.out.println();
-	    System.out.println("No errors, certificate is already trusted");
-	} catch (SSLException e) {
-	    System.out.println();
-	    e.printStackTrace(System.out);
-	}
+        BufferedReader reader =
+                new BufferedReader(new InputStreamReader(System.in));
 
-	X509Certificate[] chain = tm.chain;
-	if (chain == null) {
-	    System.out.println("Could not obtain server certificate chain");
-	    return;
-	}
+        System.out.println();
+        System.out.println("Server sent " + chain.length + " certificate(s):");
+        System.out.println();
+        MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        for (int i = 0; i < chain.length; i++) {
+            X509Certificate cert = chain[i];
+            System.out.println
+                    (" " + (i + 1) + " Subject " + cert.getSubjectDN());
+            System.out.println("   Issuer  " + cert.getIssuerDN());
+            sha1.update(cert.getEncoded());
+            System.out.println("   sha1    " + toHexString(sha1.digest()));
+            md5.update(cert.getEncoded());
+            System.out.println("   md5     " + toHexString(md5.digest()));
+            System.out.println();
+        }
 
-	BufferedReader reader =
-		new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter certificate to add to trusted keystore or 'q' to quit: [1]");
+        String line = reader.readLine().trim();
+        int k;
+        try {
+            k = (line.length() == 0) ? 0 : Integer.parseInt(line) - 1;
+        } catch (NumberFormatException e) {
+            System.out.println("KeyStore not changed");
+            return;
+        }
 
-	System.out.println();
-	System.out.println("Server sent " + chain.length + " certificate(s):");
-	System.out.println();
-	MessageDigest sha1 = MessageDigest.getInstance("SHA1");
-	MessageDigest md5 = MessageDigest.getInstance("MD5");
-	for (int i = 0; i < chain.length; i++) {
-	    X509Certificate cert = chain[i];
-	    System.out.println
-	    	(" " + (i + 1) + " Subject " + cert.getSubjectDN());
-	    System.out.println("   Issuer  " + cert.getIssuerDN());
-	    sha1.update(cert.getEncoded());
-	    System.out.println("   sha1    " + toHexString(sha1.digest()));
-	    md5.update(cert.getEncoded());
-	    System.out.println("   md5     " + toHexString(md5.digest()));
-	    System.out.println();
-	}
+        X509Certificate cert = chain[k];
+        String alias = host + "-" + (k + 1);
+        ks.setCertificateEntry(alias, cert);
 
-	System.out.println("Enter certificate to add to trusted keystore or 'q' to quit: [1]");
-	String line = reader.readLine().trim();
-	int k;
-	try {
-	    k = (line.length() == 0) ? 0 : Integer.parseInt(line) - 1;
-	} catch (NumberFormatException e) {
-	    System.out.println("KeyStore not changed");
-	    return;
-	}
+        OutputStream out = new FileOutputStream("jssecacerts");
+        ks.store(out, passphrase);
+        out.close();
 
-	X509Certificate cert = chain[k];
-	String alias = host + "-" + (k + 1);
-	ks.setCertificateEntry(alias, cert);
-
-	OutputStream out = new FileOutputStream("jssecacerts");
-	ks.store(out, "Aa30011992".toCharArray());
-	out.close();
-
-	System.out.println();
-	System.out.println(cert);
-	System.out.println();
-	System.out.println
-		("Added certificate to keystore 'jssecacerts' using alias '"
-		+ alias + "'");
+        System.out.println();
+        System.out.println(cert);
+        System.out.println();
+        System.out.println
+                ("Added certificate to keystore 'jssecacerts' using alias '"
+                        + alias + "'");
     }
 
     private static final char[] HEXDIGITS = "0123456789abcdef".toCharArray();
 
     private static String toHexString(byte[] bytes) {
-	StringBuilder sb = new StringBuilder(bytes.length * 3);
-	for (int b : bytes) {
-	    b &= 0xff;
-	    sb.append(HEXDIGITS[b >> 4]);
-	    sb.append(HEXDIGITS[b & 15]);
-	    sb.append(' ');
-	}
-	return sb.toString();
+        StringBuilder sb = new StringBuilder(bytes.length * 3);
+        for (int b : bytes) {
+            b &= 0xff;
+            sb.append(HEXDIGITS[b >> 4]);
+            sb.append(HEXDIGITS[b & 15]);
+            sb.append(' ');
+        }
+        return sb.toString();
     }
 
     private static class SavingTrustManager implements X509TrustManager {
 
-	private final X509TrustManager tm;
-	private X509Certificate[] chain;
+        private final X509TrustManager tm;
+        private X509Certificate[] chain;
 
-	SavingTrustManager(X509TrustManager tm) {
-	    this.tm = tm;
-	}
+        SavingTrustManager(X509TrustManager tm) {
+            this.tm = tm;
+        }
 
-	public X509Certificate[] getAcceptedIssuers() {
-	    throw new UnsupportedOperationException();
-	}
+        public X509Certificate[] getAcceptedIssuers() {
+	   
+	    /** 
+	     * This change has been done due to the following resolution advised for Java 1.7+
+		http://infposs.blogspot.kr/2013/06/installcert-and-java-7.html
+       	     **/ 
+	    
+	    return new X509Certificate[0];	
+            //throw new UnsupportedOperationException();
+        }
 
-	public void checkClientTrusted(X509Certificate[] chain, String authType)
-		throws CertificateException {
-	    throw new UnsupportedOperationException();
-	}
+        public void checkClientTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+            throw new UnsupportedOperationException();
+        }
 
-	public void checkServerTrusted(X509Certificate[] chain, String authType)
-		throws CertificateException {
-	    this.chain = chain;
-	    tm.checkServerTrusted(chain, authType);
-	}
+        public void checkServerTrusted(X509Certificate[] chain, String authType)
+                throws CertificateException {
+            this.chain = chain;
+            tm.checkServerTrusted(chain, authType);
+        }
     }
-
 }
